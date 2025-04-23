8d7055a45e9d65a8a0bab4645d6b90fa3cbd25f7
Nefarious46 <146811860+Nefarious46@users.noreply.github.com>
2025-04-23 10:16:16 +0300

    removed unnecessary key.pub (#140)
    * removed unnecessary key.pub
    
    * Folder to directory

--- quadlet/jwt_generate.sh
+++ quadlet/jwt_generate.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
- # Generates key.pub for authentication
- # Echoes the bearer token when ran and provides key.pub in target folder
- # Path to key.pub needs to be declared in application.properties
- # spring.security.oauth2.resourceserver.jwt.public-key-location=file:/absolute/path/in/os
+ # Generates public_key.pem for authentication
+ # Echoes the bearer token when ran and provides public_key.pem in target directory
+ # Path to public_key.pem needs to be declared in application.properties
+ # spring.security.oauth2.resourceserver.jwt.public-key-location=file:/absolute/path/in/os/public_key.pem
 
 rm -rf target
 mkdir -p target
 openssl genpkey -algorithm RSA -out target/private_key.pem -pkeyopt rsa_keygen_bits:2048
 openssl rsa -pubout -in target/private_key.pem -out target/public_key.pem
-openssl rsa -in target/private_key.pem -pubout > key.pub
 
 JWTHEADER=$(echo -n '{"alg":"RS256","typ":"JWT"}' | openssl base64 -e | tr -d '=' | tr '/+' '_-' | tr -d '\n')
 JWTPAYLOAD=$(echo -n "{\"sub\":\"subject\",\"iat\":$(date +%s)}" | openssl base64 -e | tr -d '=' | tr '/+' '_-' | tr -d '\n')
@@ -19,4 +18,5 @@ openssl dgst -sha256 -sign target/private_key.pem -out target/jwt.signature targ
 
 JWTSIGNATURE=$(openssl base64 -e -in target/jwt.signature | tr -d '=' | tr '/+' '_-' | tr -d '\n')
 
+echo ${JWTHEADER}.${JWTPAYLOAD}.${JWTSIGNATURE}
 echo ${JWTHEADER}.${JWTPAYLOAD}.${JWTSIGNATURE}
