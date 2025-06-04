04552330bfc4b56fd1ac5de2e1e3d33f4c5cf68e
Nefarious46 <146811860+Nefarious46@users.noreply.github.com>
2025-06-04 11:59:33 +0300

    Bad request > conflict on unit test

--- src/test/java/com/teragrep/cfe18/controllerTests/HubControllerTest.java
+++ src/test/java/com/teragrep/cfe18/controllerTests/HubControllerTest.java
@@ -309,7 +309,7 @@ public class HubControllerTest extends TestSpringBootInformation {
         String expected = "Hosts use the hub";
 
         assertEquals(expected, actual);
-        assertThat(deleteResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_BAD_REQUEST));
+        assertThat(deleteResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_CONFLICT));
     }
 
     @Test
