03293f1952fd62e6ac97016d7f3f90532f8ec79d
Nefarious46 <146811860+Nefarious46@users.noreply.github.com>
2026-03-23 15:21:09 +0200

    Cfe 04 transform renamed procedure occurrences

--- src/main/resources/db/migration/R__Procedure_Retrieve_transforms_for_cfe_04.sql
+++ src/main/resources/db/migration/R__Procedure_Retrieve_transforms_for_cfe_04.sql
@@ -60,7 +60,7 @@ BEGIN
 
         select t.id                 as id,
                t.cfe_04_id          as cfe_04_id,
-               t.name               as name,
+               t.transform_name               as name,
                t.write_meta         as write_meta,
                t.write_default      as write_default,
                t.default_value      as default_value,
